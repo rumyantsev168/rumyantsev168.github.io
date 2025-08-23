7185383533840b55436c8907d6ec2958cd13ba5e
rumyantsev168 <Rmisha08len@yandex.ru>
2025-08-23 21:46:17 +0300

    Major Update - v0.2.0
    - removed `job-application.html` (was cringe lol)
    - renamed and slightly edited `dummy-page.html` (new name is `test.html`)
    - `test.html` is now inaccessible from Homepage
    - added Copy button functionality in `uri-component-encoder-decoder.html` (!!)
    - renamed `script.js` to `select_and_copy.js`

---
+++ docs/assets/js/select_and_copy.js
@@ -0,0 +1,47 @@
+// original source: https://www.github.com/marcthird128/marcthird128.github.io/blob/main/docs/mazarca/assets/js/select_and_copy.js
+
+function safe_copy() {
+    let res = false;
+    try {
+        res = document.execCommand('copy');
+    } catch (e) {};
+    message(res ? 'Copied!' : 'Could not copy!');
+};
+
+function selectText(element_id) {
+    let node = document.getElementById(element_id).childNodes[0]
+    if (document.body.createTextRange) {
+        const range = document.body.createTextRange();
+        range.moveToElementText(node);
+        range.select();
+    } else if (window.getSelection) {
+        const selection = window.getSelection();
+        const range = document.createRange();
+        range.selectNodeContents(node);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    } else {
+        console.warn("Could not select text in node: Unsupported browser.");
+    };
+    safe_copy();
+};
+
+function selectTextarea(textarea_id) {
+    const textarea = document.getElementById(textarea_id);
+    textarea.select();
+    textarea.setSelectionRange(0, textarea.value.length);
+    safe_copy();
+};
+
+function message(msg) {
+    let els = document.getElementsByClassName('copied');
+    for (let i=0; i<els.length; i++) {
+        els[i].remove();
+    }
+    let el = document.createElement('div');
+    el.className = 'copied';
+    el.innerHTML = msg;
+    el.onclick = () => el.remove();
+    document.body.appendChild(el);
+    setTimeout(() => el.remove(), 1000);
+};
