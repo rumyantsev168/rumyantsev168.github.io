88e07eaa255ee87a2d40acca26983ff5a7ce14ff
rumyantsev168 <Rmisha08len@yandex.ru>
2025-09-27 15:01:46 +0300

    Trying to figure out a bug...

--- docs/source/js/main.js
+++ docs/source/js/main.js
@@ -7,10 +7,11 @@ header.appendChild(hr);
 // add footer inside main container (except for index.html)
 const main = document.getElementsByTagName("main")[0];
 let path = document.location.pathname.split("/");
+console.log(path);
 let footer = document.createElement("footer");
 footer.className = "text-center";
 if (path.at(-2) == "tests") {
-    if (path.at(-1) == "index.html") { footer.innerHTML = '<small><a href="..index.html" target="blank">Back to Homepage</a></small>' }
-    else { footer.innerHTML = '<small><a href="index.html" target="blank">Back to Tests</a></small>' };
-} else if (path.at(-1) !== "index.html") { footer.innerHTML = '<small><a href="index.html" target="blank">Back to Homepage</a></small>' };
+    if (path.at(-1) == "index.html") { footer.innerHTML = '<small><a href="..index.html">Back to Homepage</a></small>' }
+    else { footer.innerHTML = '<small><a href="index.html">Back to Tests</a></small>' };
+} else if (path.at(-1) !== "index.html") { footer.innerHTML = '<small><a href="index.html">Back to Homepage</a></small>' };
 main.appendChild(footer);
