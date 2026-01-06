ceb4230411438523b4842983b5647ef9920b60ca
rumyantsev168 <Rmisha08len@yandex.ru>
2026-01-07 01:54:59 +0300

    Update - v0.6.3
    - minor changes to chat-simulator.html
    - added count attribute to <item-slot>
    - updated 404 page

--- docs/source/js/chat.js
+++ docs/source/js/chat.js
@@ -1,35 +1,26 @@
+const chatContainer = document.getElementById("chat-container");
 const chatInput = document.getElementById("chat-input");
 const chatOutput = document.getElementById("chat-output");
-const chatClearAll = document.getElementById("chat-clear-all");
-const adminCheck = document.getElementById("adminChk");
+const adminCheck = document.getElementById("admin-chk");
 
-document.addEventListener("keydown", () => enter(event, chatInput, chatOutput));
-chatClearAll.addEventListener("click", () => clear(chatInput, chatOutput));
-
-function enter(event, chatInput, chatOutput) {
-    if (event.keyCode == 13) {
-        if (!chatInput) return;
-        let outputLine = document.createElement("p");
-        outputLine.style.margin = "0px";
-        if (adminCheck.checked) {
-            outputLine.style.color = "#dd0000";
-            outputLine.className = "admin";
-        } else { outputLine.className = "user" };
-        var newDate = new Date();
-        Date.prototype.today = function() {
-            return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
-        };
-        Date.prototype.timeNow = function() {
-             return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
-        };
-        outputLine.title = `Sent on ${newDate.today()} at ${newDate.timeNow()}`;
-        outputLine.innerText = chatInput.value;
-        chatOutput.insertBefore(outputLine, chatOutput.childNodes[0]);
-        chatInput.value = "";
+function enter(e) {
+    e.preventDefault();
+    if (!chatInput.value) return;
+    let outputLine = document.createElement("p");
+    outputLine.style.margin = "0px";
+    if (adminCheck.checked) {
+        outputLine.style.color = "#dd0000";
+        outputLine.className = "admin";
+    } else {
+        outputLine.className = "user";
     };
+    outputLine.title = new Date().toLocaleString();
+    outputLine.innerText = chatInput.value;
+    chatOutput.insertBefore(outputLine, chatOutput.childNodes[0]);
+    chatInput.value = "";
 };
 
-function clear(chatInput, chatOutput) {
+function clear() {
     chatInput.value = "";
     chatOutput.innerHTML = "";
 };
--- docs/source/js/customElements.js
+++ docs/source/js/customElements.js
@@ -1,22 +1,37 @@
 class ItemSlot extends HTMLElement {
     constructor() {
         super();
+
         const itemSlot = document.createElement("div");
         itemSlot.className = "item-slot";
-        let itemImg = null;
-        if (this.hasAttribute("large")) {
-            itemSlot.style.padding = "8px";
+
+        const isLarge = this.hasAttribute("large");
+        const itemSrc = this.getAttribute("item");
+        const itemCount = this.getAttribute("count");
+        const itemTooltip = this.getAttribute("tooltip");
+
+        if (isLarge) {
+            itemSlot.style.padding = "4px";
         };
-        if (this.getAttribute("item")) {
-            itemImg = document.createElement("img");
-            itemImg.src = this.getAttribute("item");
+
+        if (itemSrc) {
+            const itemImg = document.createElement("img");
+            itemImg.src = itemSrc;
             itemImg.width = 32;
             itemImg.height = 32;
             itemSlot.appendChild(itemImg);
         };
-        if (this.getAttribute("tooltip")) {
-            itemSlot.title = this.getAttribute("tooltip");
+
+        if (itemCount) {
+            const itemCountSpan = document.createElement("span");
+            itemCountSpan.innerText = itemCount;
+            itemSlot.appendChild(itemCountSpan);
         };
+
+        if (itemTooltip) {
+            itemSlot.title = itemTooltip;
+        };
+
         this.appendChild(itemSlot);
     };
 };
